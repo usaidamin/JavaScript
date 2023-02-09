d76b0463a07dcef88f9049a87ac6f4959eacb664
Muhammad Usaid <usaidamin21@gmail.com>
2023-02-09 15:59:48 +0500

    Learning JavaScript

---
+++ app.js
@@ -0,0 +1,108 @@
+// alert("HELLO WORLD");
+
+// Variables
+
+// var myname = " Muhammad Usaid";
+
+// alert(myname);
+
+// Data Types
+
+// String
+// Number
+// Boolean
+// Undefined 
+// Null
+
+// var fullName = "Jaffar Aman";  //STRING
+// var age = 21;   //NUMBER
+// var contactNumber = "03462528569";  //STRING
+// var email = "amanjaffar50@gmail.com"; //STRING
+// var isActive = true; //BOOL
+// var result = undefined; //UNDEFINED
+// var relationStatus = null; //NULL
+
+// alert(fullName);
+// alert(isActive);
+
+// var firstName = "Jaffar";
+// var lastName = "Amin";
+
+// var fullName = firstName + " " + lastName
+// console.log(fullName)
+
+// var a = "10";  // string
+// var b = 20;  // number
+
+// var sum = a + b;
+
+// console.log(sum)
+
+// var value = "50" + 100  //concat   + sign
+// console.log(value)
+
+
+// + , - , * , / , %
+// var a = "10";
+// var b = 20;
+
+// //ADD
+// var add = a + b
+// console.log(add)
+
+
+// //SUB
+// var sub = a - b
+// console.log(sub)
+
+// //MUL
+
+// var mul = a * b
+// console.log(mul)
+
+// //DIV
+// var div = a / b
+// console.log(div)
+
+
+
+// var reminder = 9 % 2  // 0
+// console.log(reminder)
+
+
+
+// var value = 10 + 20 - 64;
+// var value = 10 + (10 - 30) - 20 + 10
+//           = 10 + (-20) -20 +10
+//            = 10 - 20 -20 +10
+//            = 20 - 20 - 20
+//             0 -20
+//             -20
+
+// console.log(value)
+
+// var value2 = (10 - 20 * 2) + 20 - 10 * 5
+//   =  (-30) - 30
+//   = -30 - 30
+//    = -60
+// console.log(value2)
+
+// var value = "Hello" + 10 + 20;
+
+// var value2 = 20 + "HELLO" + 10
+// var value3 = "10" + 20 + "HELLO"
+// var value4 = "HELLO" + (10 + 20)
+
+// console.log(value)
+
+var a = 15;
+
+var value1 = a++ + ++a - --a;
+         //= 15  + 17  - 16 = 16 Answer
+
+console.log(value1)
+
+var value2 = ++a + ++a - --a + a-- + a - a - a++ - --a;
+         //= 16  + 17  - 16  + 16  + 15 - 15 - 15 - 15 = 3 Answer
+
+console.log(value2)
